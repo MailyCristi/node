dd838ec37d24005632d8301d5c5bda71f8200c51
MailyCristi <cristimi1232@gmail.com>
2024-07-10 01:14:51 -0400

    commit2

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('¡Hola Mundo!');
-});
+app.use(express.static('public'));
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
